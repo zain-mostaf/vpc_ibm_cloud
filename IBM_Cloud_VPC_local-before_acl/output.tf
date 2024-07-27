9e0d64a4396c6229ee34fd2ee43f34f7830b99eb
Zain Mostafa <Zain.Mostafa@ibm.com>
2024-07-28 02:21:25 +0300

    first commit

--- IBM_Cloud_VPC_local-before_acl/output.tf
+++ IBM_Cloud_VPC_local-before_acl/output.tf
@@ -13,7 +13,9 @@ output "address_prefix_id" {
   value       = module.address_prefix.id
 }
 
-output "security_group_rules" {
-  description = "All the IDs of Security group Rules"
-  value       = [for rule in ibm_is_security_group_rule.sg_rules : rule.id]
-}
+
+
+
+#output "subnet_id" {
+#  value = module.subnet.subnet_id
+#}
