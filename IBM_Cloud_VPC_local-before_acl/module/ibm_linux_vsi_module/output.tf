367c1a871eafb1d44f032c1539671ea08a607942
Zain Mostafa <Zain.Mostafa@ibm.com>
2024-07-28 22:04:50 +0300

    first commit

--- IBM_Cloud_VPC_local-before_acl/module/ibm_linux_vsi_module/output.tf
+++ IBM_Cloud_VPC_local-before_acl/module/ibm_linux_vsi_module/output.tf
@@ -1,3 +1,8 @@
 output "linux_private_ips" {
   value = [for instance in ibm_is_instance.linux_instance : instance.primary_network_interface[0].primary_ip]
 }
+
+output "security_group_id" {
+  description = "The ID of the Security group"
+  value       = var.create_security_group ?  data.ibm_is_security_group.sg_ds.0.id : ibm_is_security_group.sg[0].id 
+}
