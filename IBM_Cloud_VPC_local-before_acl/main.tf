b2d87e64e3e38b7697c9e066925e8415ad1ae349
Zain Mostafa <Zain.Mostafa@ibm.com>
2024-07-28 23:54:39 +0300

    first commit

--- IBM_Cloud_VPC_local-before_acl/main.tf
+++ IBM_Cloud_VPC_local-before_acl/main.tf
@@ -102,6 +102,6 @@ module "linux_instances" {
   vpc_id              = module.vpc.vpc_id
   zone                = var.zone
   subnet_id           = module.subnet[0].subnet_id
-  security_group_ids  = [module.security_group.security_group_id]
+  #security_group_id  = [module.security_group.security_group_id]
   ssh_key_id          = module.ssh_key.ssh_key_id
 }
